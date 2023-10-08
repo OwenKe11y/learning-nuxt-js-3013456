c422a09d6d61eabdc432658bf3f266f47008640c
OwenKe11y <owenkellysch@gmail.com>
2023-10-08 21:10:24 +0100

    added padding

--- pages/About.vue
+++ pages/About.vue
@@ -1,12 +1,18 @@
 <template>
   <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-8 sm:pb-8"
   >
     <link
       href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
       rel="stylesheet"
     />
+
     <div>
+      <div class="flex justify-center pt-8 sm:pt-0">
+        <a href="https://nuxtjs.org" target="_blank">
+          <NuxtLogo />
+        </a>
+      </div>
       <div class="mt-6 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <Header />
       </div>
