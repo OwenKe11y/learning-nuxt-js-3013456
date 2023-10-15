8ed3423dba10b14f860a295b9af557e9833d3b02
OwenKe11y <owenkellysch@gmail.com>
2023-10-15 12:06:41 +0100

    fetching data

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Hello World!</h1>
+  <div>
+    <h1>Most Popular Rivers in the World</h1>
+    <ul>
+      <li v-for="(river, index) in rivers" :key="`river-${index}`">
+        {{ river.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  // async asyncData ({ $http }) {
+  //   const res = await $http.$get('https://api.nuxtjs.dev/rivers')
+  //   return {
+  //     rivers: res
+  //   }
+  // },
+
+  data () {
+    return {
+      rivers: []
+    }
+  },
+  async fetch () {
+    const res = await this.$http.$get('https://api.nuxtjs.dev/rivers')
+    this.rivers = res
+  }
 }
 </script>
